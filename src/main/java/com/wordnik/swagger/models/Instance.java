3ddeff500078c04fce62eebb6a93f205a877a665
Tony Tam <fehguy@gmail.com>
2014-09-25 16:59:57 -0400

    added validation

---
+++ src/main/java/com/wordnik/swagger/models/Instance.java
@@ -0,0 +1,12 @@
+package com.wordnik.swagger.models;
+
+public class Instance {
+  private String pointer;
+
+  public String getPointer() {
+    return pointer;
+  }
+  public void setPointer(String pointer) {
+    this.pointer = pointer;
+  }
+}
