84779f46eee7f0fac90e54c6d94f1de5d71132dc
MadusankaP <rmmpremaratne@gmail.com>
2015-03-23 11:24:23 +0530

    Code formatted to be read easily

--- src/main/java/com/wordnik/swagger/models/SchemaValidationError.java
+++ src/main/java/com/wordnik/swagger/models/SchemaValidationError.java
@@ -1,117 +1,126 @@
 package com.wordnik.swagger.models;
 
 import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.deser.*; 
 import com.fasterxml.jackson.databind.node.*;
 
 import java.util.*;
 
 public class SchemaValidationError {
-  private String level, domain, keyword, message;
-  private Schema schema;
-  private Instance instance;
-  private List<String> required;
-  private List<String> missing;
+    private String level, domain, keyword, message;
+    private Schema schema;
+    private Instance instance;
+    private List<String> required;
+    private List<String> missing;
 
-  public SchemaValidationError() {}
-  public SchemaValidationError(JsonNode node) {
-    JsonNode prop = node.get("level");
-    if(prop != null)
-      level = (String) ((TextNode) prop).asText();
-
-    prop = node.get("domain");
-    if(prop != null)
-      domain = (String) ((TextNode) prop).asText();
-
-    prop = node.get("keyword");
-    if(prop != null)
-      keyword = (String) ((TextNode) prop).asText();
-
-    prop = node.get("message");
-    if(prop != null)
-      message = (String) ((TextNode) prop).asText();
-
-    prop = node.get("schema");
-    if(prop != null) {
-      schema = new Schema();
-      JsonNode s = (JsonNode) prop;
-      prop = s.get("loadingURI");
-      if(prop != null)
-        schema.setLoadingURI((String) ((TextNode) prop).asText());
-      prop = s.get("pointer");
-      if(prop != null)
-        schema.setPointer((String) ((TextNode) prop).asText());
+    public SchemaValidationError() {
     }
 
-    prop = node.get("instance");
-    if(prop != null) {
-      instance = new Instance();
-      JsonNode s = (JsonNode) prop;
-      prop = s.get("pointer");
-      if(prop != null)
-        instance.setPointer((String) ((TextNode) prop).asText());
+    public SchemaValidationError(JsonNode node) {
+        JsonNode prop = node.get("level");
+        if (prop != null)
+            level = (String) ((TextNode) prop).asText();
+
+        prop = node.get("domain");
+        if (prop != null)
+            domain = (String) ((TextNode) prop).asText();
+
+        prop = node.get("keyword");
+        if (prop != null)
+            keyword = (String) ((TextNode) prop).asText();
+
+        prop = node.get("message");
+        if (prop != null)
+            message = (String) ((TextNode) prop).asText();
+
+        prop = node.get("schema");
+        if (prop != null) {
+            schema = new Schema();
+            JsonNode s = (JsonNode) prop;
+            prop = s.get("loadingURI");
+            if (prop != null)
+                schema.setLoadingURI((String) ((TextNode) prop).asText());
+            prop = s.get("pointer");
+            if (prop != null)
+                schema.setPointer((String) ((TextNode) prop).asText());
+        }
+
+        prop = node.get("instance");
+        if (prop != null) {
+            instance = new Instance();
+            JsonNode s = (JsonNode) prop;
+            prop = s.get("pointer");
+            if (prop != null)
+                instance.setPointer((String) ((TextNode) prop).asText());
+        }
+
+        prop = node.get("required");
+        if (prop != null) {
+            ArrayNode an = (ArrayNode) prop;
+        }
+
     }
-    
-    prop = node.get("required");
-    if(prop != null) {
-      ArrayNode an = (ArrayNode) prop;
+
+    public String getLevel() {
+        return level;
     }
 
-  }
+    public void setLevel(String level) {
+        this.level = level;
+    }
 
-  public String getLevel() {
-    return level;
-  }
-  public void setLevel(String level) {
-    this.level = level;
-  }
+    public String getDomain() {
+        return domain;
+    }
 
-  public String getDomain() {
-    return domain;
-  }
-  public void setDomain(String domain) {
-    this.domain = domain;
-  }
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
 
-  public String getKeyword() {
-    return keyword;
-  }
-  public void setKeyword(String keyword) {
-    this.keyword = keyword;
-  }
+    public String getKeyword() {
+        return keyword;
+    }
 
-  public String getMessage() {
-    return message;
-  }
-  public void setMessage(String message) {
-    this.message = message;
-  }
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
 
-  public List<String> getRequired() {
-    return required;
-  }
-  public void setRequired(List<String> required) {
-    this.required = required;
-  }
+    public String getMessage() {
+        return message;
+    }
 
-  public List<String> getMissing() {
-    return missing;
-  }
-  public void setMissing(List<String> missing) {
-    this.missing = missing;
-  }
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-  public Schema getSchema() {
-    return schema;
-  }
-  public void setSchema(Schema schema) {
-    this.schema = schema;
-  }
+    public List<String> getRequired() {
+        return required;
+    }
 
-  public Instance getInstance() {
-    return instance;
-  }
-  public void setInstance(Instance instance) {
-    this.instance = instance;
-  }
+    public void setRequired(List<String> required) {
+        this.required = required;
+    }
+
+    public List<String> getMissing() {
+        return missing;
+    }
+
+    public void setMissing(List<String> missing) {
+        this.missing = missing;
+    }
+
+    public Schema getSchema() {
+        return schema;
+    }
+
+    public void setSchema(Schema schema) {
+        this.schema = schema;
+    }
+
+    public Instance getInstance() {
+        return instance;
+    }
+
+    public void setInstance(Instance instance) {
+        this.instance = instance;
+    }
 }
--- src/main/java/com/wordnik/swagger/services/ValidatorService.java
+++ src/main/java/com/wordnik/swagger/services/ValidatorService.java
@@ -23,122 +23,116 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ValidatorService {
-  static String SCHEMA_FILE = "schema.json";
-  static String SCHEMA_URL = "http://swagger.io/v2/schema.json";
-  static ObjectMapper MAPPER = new ObjectMapper();
-  private static final Logger log = Logger.getLogger(ValidatorService.class);
+    static String SCHEMA_FILE = "schema.json";
+    static String SCHEMA_URL = "http://swagger.io/v2/schema.json";
+    static ObjectMapper MAPPER = new ObjectMapper();
+    private static final Logger log = Logger.getLogger(ValidatorService.class);
 
-  public void validateByUrl(HttpServletRequest request, HttpServletResponse response, String url) {
-    if(url == null) {
-      fail(response);
+    public void validateByUrl(HttpServletRequest request, HttpServletResponse response, String url) {
+        if (url == null) {
+            fail(response);
+        } else {
+            try {
+                String inputDoc = getUrlContents(url);
+                String schemaText = getSchema();
+                JsonNode schemaObject = MAPPER.readTree(schemaText);
+                JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+                JsonSchema schema = factory.getJsonSchema(schemaObject);
+                ProcessingReport report = schema.validate(JsonLoader.fromString(inputDoc));
+                if (report.isSuccess())
+                    success(response);
+                else
+                    fail(response);
+            } catch (Exception e) {
+                error(response);
+            }
+        }
     }
-    else {
-      try {
+
+    public List<SchemaValidationError> debugByUrl(String url) throws Exception {
         String inputDoc = getUrlContents(url);
+        return debugByContent(inputDoc);
+    }
+
+    public List<SchemaValidationError> debugByContent(String content) throws Exception {
         String schemaText = getSchema();
         JsonNode schemaObject = MAPPER.readTree(schemaText);
         JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
         JsonSchema schema = factory.getJsonSchema(schemaObject);
-        ProcessingReport report = schema.validate(JsonLoader.fromString(inputDoc));
-        if(report.isSuccess())
-          success(response);
-        else
-          fail(response);
-      }
-      catch (Exception e) {
-        error(response);
-      }
+        ProcessingReport report = schema.validate(JsonLoader.fromString(content));
+        ListProcessingReport lp = new ListProcessingReport();
+        lp.mergeWith(report);
+
+        List<SchemaValidationError> output = new ArrayList<SchemaValidationError>();
+        for (ProcessingMessage pm : lp) {
+            output.add(new SchemaValidationError(pm.asJson()));
+        }
+        return output;
     }
-  }
 
-  public List<SchemaValidationError> debugByUrl(String url) throws Exception {
-    String inputDoc = getUrlContents(url);
-    return debugByContent(inputDoc);
-  }
-
-  public List<SchemaValidationError> debugByContent(String content) throws Exception {
-    String schemaText = getSchema();
-    JsonNode schemaObject = MAPPER.readTree(schemaText);
-    JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
-    JsonSchema schema = factory.getJsonSchema(schemaObject);
-    ProcessingReport report = schema.validate(JsonLoader.fromString(content));
-    ListProcessingReport lp = new ListProcessingReport();
-    lp.mergeWith(report);
-
-    List<SchemaValidationError> output = new ArrayList<SchemaValidationError>();
-      for (ProcessingMessage pm : lp) {
-          output.add(new SchemaValidationError(pm.asJson()));
-      }
-    return output;
-  }
-
-  private void success(HttpServletResponse response) {
-    try {
-      String name = "valid.png";
-      InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
-      if(is != null) {
-        IOUtils.copy(is, response.getOutputStream());
-      }
+    private void success(HttpServletResponse response) {
+        try {
+            String name = "valid.png";
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
+            if (is != null) {
+                IOUtils.copy(is, response.getOutputStream());
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
-    catch(IOException e) {
-        log.error(e.getMessage());
-    }
-  }
 
-  private void error(HttpServletResponse response) {
-    try {
-      String name = "error.png";
-      InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
-      if(is != null) {
-        IOUtils.copy(is, response.getOutputStream());
-      }
+    private void error(HttpServletResponse response) {
+        try {
+            String name = "error.png";
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
+            if (is != null) {
+                IOUtils.copy(is, response.getOutputStream());
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
-    catch(IOException e) {
-        log.error(e.getMessage());
-    }
-  }
 
-  private void fail(HttpServletResponse response) {
-    try {
-      String name = "invalid.png";
-      InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
-      if(is != null) {
-        IOUtils.copy(is, response.getOutputStream());
-      }
+    private void fail(HttpServletResponse response) {
+        try {
+            String name = "invalid.png";
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
+            if (is != null) {
+                IOUtils.copy(is, response.getOutputStream());
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
     }
-    catch(IOException e) {
-        log.error(e.getMessage());
+
+    private String getSchema() throws Exception {
+        try {
+            return getUrlContents(SCHEMA_URL);
+        } catch (Exception e) {
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(SCHEMA_FILE);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(is));
+
+            String inputLine;
+            StringBuilder contents = new StringBuilder();
+            while ((inputLine = in.readLine()) != null)
+                contents.append(inputLine);
+            in.close();
+            return contents.toString();
+        }
     }
-  }
 
-  private String getSchema() throws Exception {
-    try{
-      return getUrlContents(SCHEMA_URL);
+    private String getUrlContents(String urlString) throws Exception {
+        URL url = new URL(urlString);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(url.openStream()));
+
+        String inputLine;
+        StringBuilder contents = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            contents.append(inputLine);
+        in.close();
+        return contents.toString();
     }
-    catch (Exception e) {
-      InputStream is = this.getClass().getClassLoader().getResourceAsStream(SCHEMA_FILE);
-      BufferedReader in = new BufferedReader(
-        new InputStreamReader(is));
-
-      String inputLine;
-      StringBuilder contents = new StringBuilder();
-      while ((inputLine = in.readLine()) != null)
-        contents.append(inputLine);
-      in.close();
-      return contents.toString();
-    }
-  }
-
-  private String getUrlContents(String urlString) throws Exception {
-    URL url = new URL(urlString);
-    BufferedReader in = new BufferedReader(
-      new InputStreamReader(url.openStream()));
-
-    String inputLine;
-    StringBuilder contents = new StringBuilder();
-    while ((inputLine = in.readLine()) != null)
-      contents.append(inputLine);
-    in.close();
-    return contents.toString();
-  }
 }
