eb5d17afd5d56393c547a70a7d41e905135a51b9
Madusanka <rmmpremaratne@gmail.com>
2015-04-07 17:54:55 +0530

    Code formatting done

--- src/main/java/com/wordnik/swagger/resources/ValidatorResource.java
+++ src/main/java/com/wordnik/swagger/resources/ValidatorResource.java
@@ -15,61 +15,58 @@ import java.util.*;
 @Path("/")
 @Api(value = "/validator", description = "Validator for Swagger Specs")
 public class ValidatorResource {
-  ValidatorService service = new ValidatorService();
+    ValidatorService service = new ValidatorService();
 
-  @GET
-  @ApiOperation(value = "Validates a spec based on a URL")
-  @ApiResponses(value = {  })
-  @Produces({"image/png"})
-  public Response validateByUrl(
-    @Context HttpServletRequest request,
-    @Context HttpServletResponse response,
-    @ApiParam(value = "url of spec to validate" ) @QueryParam("url")String url) throws WebApplicationException {
-    try {
-      service.validateByUrl(request, response, url);
-      response.addHeader("Cache-Control", "no-cache");
+    @GET
+    @ApiOperation(value = "Validates a spec based on a URL")
+    @ApiResponses(value = {})
+    @Produces({"image/png"})
+    public Response validateByUrl(
+            @Context HttpServletRequest request,
+            @Context HttpServletResponse response,
+            @ApiParam(value = "url of spec to validate") @QueryParam("url") String url) throws WebApplicationException {
+        try {
+            service.validateByUrl(request, response, url);
+            response.addHeader("Cache-Control", "no-cache");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Response.ok().build();
     }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
-    return Response.ok().build();
-  }
 
-  @GET
-  @Path("/debug")
-  @Produces({"application/json"})
-  @ApiOperation(value = "Validates a spec based on a URL",
-    response = SchemaValidationError.class,
-    responseContainer = "List")
-  @ApiResponses(value = {  })
-  public Response debugByUrl(
-    @Context HttpServletRequest request,
-    @Context HttpServletResponse response,
-    @ApiParam(value = "url of spec to validate" ) @QueryParam("url")String url) throws WebApplicationException {
-    try {
-      return Response.ok().entity(service.debugByUrl(request, response, url)).build();
+    @GET
+    @Path("/debug")
+    @Produces({"application/json"})
+    @ApiOperation(value = "Validates a spec based on a URL",
+            response = SchemaValidationError.class,
+            responseContainer = "List")
+    @ApiResponses(value = {})
+    public Response debugByUrl(
+            @Context HttpServletRequest request,
+            @Context HttpServletResponse response,
+            @ApiParam(value = "url of spec to validate") @QueryParam("url") String url) throws WebApplicationException {
+        try {
+            return Response.ok().entity(service.debugByUrl(request, response, url)).build();
+        } catch (Exception e) {
+            return Response.status(500).build();
+        }
     }
-    catch (Exception e) {
-      return Response.status(500).build();
-    }
-  }
 
-  @POST
-  @Path("/debug")
-  @Produces({"application/json"})
-  @ApiOperation(value = "Validates a spec based on a URL",
-    response = SchemaValidationError.class,
-    responseContainer = "List")
-  @ApiResponses(value = {  })
-  public Response debugByContent(
-    @Context HttpServletRequest request,
-    @Context HttpServletResponse response,
-    @ApiParam(value = "spec contents" )String spec) throws WebApplicationException {
-    try {
-      return Response.ok().entity(service.debugByContent(request, response, spec)).build();
+    @POST
+    @Path("/debug")
+    @Produces({"application/json"})
+    @ApiOperation(value = "Validates a spec based on a URL",
+            response = SchemaValidationError.class,
+            responseContainer = "List")
+    @ApiResponses(value = {})
+    public Response debugByContent(
+            @Context HttpServletRequest request,
+            @Context HttpServletResponse response,
+            @ApiParam(value = "spec contents") String spec) throws WebApplicationException {
+        try {
+            return Response.ok().entity(service.debugByContent(request, response, spec)).build();
+        } catch (Exception e) {
+            return Response.status(500).build();
+        }
     }
-    catch (Exception e) {
-      return Response.status(500).build();
-    }
-  }
 }
--- src/main/java/com/wordnik/swagger/services/ValidatorService.java
+++ src/main/java/com/wordnik/swagger/services/ValidatorService.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.commons.io.IOUtils;
 
-import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 import com.github.fge.jsonschema.main.*;
 import com.github.fge.jsonschema.core.report.*;
 import com.github.fge.jackson.JsonLoader;
@@ -21,177 +20,169 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class ValidatorService {
-  static String SCHEMA_FILE = "schema.json";
-  static String SCHEMA_URL = "http://swagger.io/v2/schema.json";
-  static ObjectMapper MAPPER = new ObjectMapper();
-  Logger LOGGER = LoggerFactory.getLogger(ValidatorService.class);
-  private JsonSchema schema;
+    static String SCHEMA_FILE = "schema.json";
+    static String SCHEMA_URL = "http://swagger.io/v2/schema.json";
+    static ObjectMapper MAPPER = new ObjectMapper();
+    Logger LOGGER = LoggerFactory.getLogger(ValidatorService.class);
+    private JsonSchema schema;
 
-  public void validateByUrl(HttpServletRequest request, HttpServletResponse response, String url) {
-    System.out.println("validationUrl: " + url + ", forClient: " + getRemoteAddr(request) + ", method: get");
-    if(url == null) {
-      fail(response);
-    }
-    else {
-      try {
-        String inputDoc = getUrlContents(url);
+    public void validateByUrl(HttpServletRequest request, HttpServletResponse response, String url) {
+        System.out.println("validationUrl: " + url + ", forClient: " + getRemoteAddr(request) + ", method: get");
+        if (url == null) {
+            fail(response);
+        } else {
+            try {
+                String inputDoc = getUrlContents(url);
 
-        if (schema == null) {
-          String schemaText = getSchema();
-          JsonNode schemaObject = MAPPER.readTree(schemaText);
-          JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
-          schema = factory.getJsonSchema(schemaObject);
+                if (schema == null) {
+                    String schemaText = getSchema();
+                    JsonNode schemaObject = MAPPER.readTree(schemaText);
+                    JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+                    schema = factory.getJsonSchema(schemaObject);
+                }
+                ProcessingReport report = schema.validate(JsonLoader.fromString(inputDoc));
+                if (report.isSuccess()) {
+                    Swagger swagger = Json.mapper().readValue(inputDoc, Swagger.class);
+                    if (swagger != null) {
+                        System.out.println("swaggerHost: " + swagger.getHost() + ", forClient: " + getRemoteAddr(request));
+                    }
+                    success(response);
+                } else
+                    fail(response);
+            } catch (Exception e) {
+                error(response);
+            }
         }
-        ProcessingReport report = schema.validate(JsonLoader.fromString(inputDoc));
-        if(report.isSuccess()) {
-          Swagger swagger = Json.mapper().readValue(inputDoc, Swagger.class);
-          if(swagger != null) {
-            System.out.println("swaggerHost: " + swagger.getHost() + ", forClient: " + getRemoteAddr(request));
-          }
-          success(response);
+    }
+
+    public List<SchemaValidationError> debugByUrl(HttpServletRequest request, HttpServletResponse response, String url) throws Exception {
+        System.out.println("validationUrl: " + url + ", forClient: " + getRemoteAddr(request) + ", method: get, debug: true");
+        String content = getUrlContents(url);
+        String schemaText = getSchema();
+        JsonNode schemaObject = MAPPER.readTree(schemaText);
+        JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+        JsonSchema schema = factory.getJsonSchema(schemaObject);
+        ProcessingReport report = schema.validate(JsonLoader.fromString(content));
+        ListProcessingReport lp = new ListProcessingReport();
+        lp.mergeWith(report);
+
+        if (report.isSuccess()) {
+            Swagger swagger = Json.mapper().readValue(content, Swagger.class);
+            if (swagger != null) {
+                System.out.println("swaggerHost: " + swagger.getHost() + ", forClient: " + getRemoteAddr(request));
+            }
         }
-        else
-          fail(response);
-      }
-      catch (Exception e) {
-        error(response);
-      }
-    }
-  }
 
-  public List<SchemaValidationError> debugByUrl(HttpServletRequest request, HttpServletResponse response, String url) throws Exception {
-    System.out.println("validationUrl: " + url + ", forClient: " + getRemoteAddr(request) + ", method: get, debug: true");
-    String content = getUrlContents(url);
-    String schemaText = getSchema();
-    JsonNode schemaObject = MAPPER.readTree(schemaText);
-    JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
-    JsonSchema schema = factory.getJsonSchema(schemaObject);
-    ProcessingReport report = schema.validate(JsonLoader.fromString(content));
-    ListProcessingReport lp = new ListProcessingReport();
-    lp.mergeWith(report);
-
-    if(report.isSuccess()) {
-      Swagger swagger = Json.mapper().readValue(content, Swagger.class);
-      if(swagger != null) {
-        System.out.println("swaggerHost: " + swagger.getHost() + ", forClient: " + getRemoteAddr(request));
-      }
+        List<SchemaValidationError> output = new ArrayList<SchemaValidationError>();
+        java.util.Iterator<ProcessingMessage> it = lp.iterator();
+        while (it.hasNext()) {
+            ProcessingMessage pm = it.next();
+            output.add(new SchemaValidationError(pm.asJson()));
+        }
+        return output;
     }
 
-    List<SchemaValidationError> output = new ArrayList<SchemaValidationError>();
-    java.util.Iterator<ProcessingMessage> it = lp.iterator();
-    while(it.hasNext()) {
-      ProcessingMessage pm = it.next();
-      output.add(new SchemaValidationError(pm.asJson()));
-    }
-    return output;
-  }
+    public List<SchemaValidationError> debugByContent(HttpServletRequest request, HttpServletResponse response, String content) throws Exception {
+        System.out.println("validationUrl: n/a, forClient: " + getRemoteAddr(request) + ", method: post, debug: true");
+        String schemaText = getSchema();
+        JsonNode schemaObject = MAPPER.readTree(schemaText);
+        JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+        JsonSchema schema = factory.getJsonSchema(schemaObject);
+        ProcessingReport report = schema.validate(JsonLoader.fromString(content));
+        ListProcessingReport lp = new ListProcessingReport();
+        lp.mergeWith(report);
 
-  public List<SchemaValidationError> debugByContent(HttpServletRequest request, HttpServletResponse response, String content) throws Exception {
-    System.out.println("validationUrl: n/a, forClient: " + getRemoteAddr(request) + ", method: post, debug: true");
-    String schemaText = getSchema();
-    JsonNode schemaObject = MAPPER.readTree(schemaText);
-    JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
-    JsonSchema schema = factory.getJsonSchema(schemaObject);
-    ProcessingReport report = schema.validate(JsonLoader.fromString(content));
-    ListProcessingReport lp = new ListProcessingReport();
-    lp.mergeWith(report);
+        if (report.isSuccess()) {
+            Swagger swagger = Json.mapper().readValue(content, Swagger.class);
+            if (swagger != null) {
+                System.out.println("swaggerHost: " + swagger.getHost() + ", forClient: " + getRemoteAddr(request));
+            }
+        }
 
-    if(report.isSuccess()) {
-      Swagger swagger = Json.mapper().readValue(content, Swagger.class);
-      if(swagger != null) {
-        System.out.println("swaggerHost: " + swagger.getHost() + ", forClient: " + getRemoteAddr(request));
-      }
-    }
-
-    List<SchemaValidationError> output = new ArrayList<SchemaValidationError>();
-    java.util.Iterator<ProcessingMessage> it = lp.iterator();
-    while(it.hasNext()) {
-      ProcessingMessage pm = it.next();
-      output.add(new SchemaValidationError(pm.asJson()));
-    }
-    return output;
-  }
-
-  private void success(HttpServletResponse response) {
-    try {
-      String name = "valid.png";
-      InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
-      if(is != null) {
-        IOUtils.copy(is, response.getOutputStream());
-      }
-    }
-    catch(IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void error(HttpServletResponse response) {
-    try {
-      String name = "error.png";
-      InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
-      if(is != null) {
-        IOUtils.copy(is, response.getOutputStream());
-      }
-    }
-    catch(IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void fail(HttpServletResponse response) {
-    try {
-      String name = "invalid.png";
-      InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
-      if(is != null) {
-        IOUtils.copy(is, response.getOutputStream());
-      }
-    }
-    catch(IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private String getSchema() throws Exception {
-    try{
-      return getUrlContents(SCHEMA_URL);
-    }
-    catch (Exception e) {
-      InputStream is = this.getClass().getClassLoader().getResourceAsStream(SCHEMA_FILE);
-      BufferedReader in = new BufferedReader(
-        new InputStreamReader(is));
-
-      String inputLine;
-      StringBuilder contents = new StringBuilder();
-      while ((inputLine = in.readLine()) != null)
-        contents.append(inputLine);
-      in.close();
-      return contents.toString();
-    }
-  }
-
-  private String getUrlContents(String urlString) throws Exception {
-    URL url = new URL(urlString);
-    BufferedReader in = new BufferedReader(
-      new InputStreamReader(url.openStream()));
-
-    String inputLine;
-    StringBuilder contents = new StringBuilder();
-    while ((inputLine = in.readLine()) != null)
-      contents.append(inputLine);
-    in.close();
-    return contents.toString();
-  }
-
-  protected String getRemoteAddr(HttpServletRequest request) {
-    String ipAddress = request.getHeader("X-FORWARDED-FOR");  
-    if (ipAddress == null) {  
-      ipAddress = request.getRemoteAddr();
+        List<SchemaValidationError> output = new ArrayList<SchemaValidationError>();
+        java.util.Iterator<ProcessingMessage> it = lp.iterator();
+        while (it.hasNext()) {
+            ProcessingMessage pm = it.next();
+            output.add(new SchemaValidationError(pm.asJson()));
+        }
+        return output;
     }
 
-    return ipAddress;
-  }
+    private void success(HttpServletResponse response) {
+        try {
+            String name = "valid.png";
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
+            if (is != null) {
+                IOUtils.copy(is, response.getOutputStream());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void error(HttpServletResponse response) {
+        try {
+            String name = "error.png";
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
+            if (is != null) {
+                IOUtils.copy(is, response.getOutputStream());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fail(HttpServletResponse response) {
+        try {
+            String name = "invalid.png";
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(name);
+            if (is != null) {
+                IOUtils.copy(is, response.getOutputStream());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getSchema() throws Exception {
+        try {
+            return getUrlContents(SCHEMA_URL);
+        } catch (Exception e) {
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(SCHEMA_FILE);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(is));
+
+            String inputLine;
+            StringBuilder contents = new StringBuilder();
+            while ((inputLine = in.readLine()) != null)
+                contents.append(inputLine);
+            in.close();
+            return contents.toString();
+        }
+    }
+
+    private String getUrlContents(String urlString) throws Exception {
+        URL url = new URL(urlString);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(url.openStream()));
+
+        String inputLine;
+        StringBuilder contents = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            contents.append(inputLine);
+        in.close();
+        return contents.toString();
+    }
+
+    protected String getRemoteAddr(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-FORWARDED-FOR");
+        if (ipAddress == null) {
+            ipAddress = request.getRemoteAddr();
+        }
+
+        return ipAddress;
+    }
 }
