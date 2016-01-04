dc88af4d617ca13824e3bc022c1c62969d824373
Tony Tam <fehguy@gmail.com>
2016-01-04 11:07:09 -0800

    exposed new parser output for better feedback to end-users

--- src/main/java/io/swagger/validator/services/ValidatorService.java
+++ src/main/java/io/swagger/validator/services/ValidatorService.java
@@ -8,14 +8,15 @@ import com.github.fge.jsonschema.core.report.ProcessingMessage;
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
-import io.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
 import io.swagger.parser.util.SwaggerDeserializationResult;
 import io.swagger.util.Json;
 import io.swagger.util.Yaml;
+import io.swagger.validator.models.ResponsePayload;
 import io.swagger.validator.models.SchemaValidationError;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
@@ -39,15 +40,15 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ValidatorService {
+    static final String INVALID_VERSION = "Deprecated Swagger version.  Please visit http://swagger.io for information on upgrading to Swagger 2.0\"";
+    static final String SCHEMA_FILE = "schema.json";
+    static final String SCHEMA_URL = "http://swagger.io/v2/schema.json";
+
     static Logger LOGGER = LoggerFactory.getLogger(ValidatorService.class);
     static long LAST_FETCH = 0;
     static String CACHED_SCHEMA = null;
-    static String SCHEMA_FILE = "schema.json";
-    static String SCHEMA_URL = "http://swagger.io/v2/schema.json";
     static ObjectMapper JsonMapper = Json.mapper();
     static ObjectMapper YamlMapper = Yaml.mapper();
     private JsonSchema schema;
@@ -55,42 +56,28 @@ public class ValidatorService {
     public void validateByUrl(HttpServletRequest request, HttpServletResponse response, String url) {
         LOGGER.info("validationUrl: " + url + ", forClient: " + getRemoteAddr(request));
 
-        if (url == null) {
-            LOGGER.debug("no URL passed");
-            fail(response);
-        } else {
-            try {
-                String inputDoc = getUrlContents(url);
+        ResponsePayload payload = null;
 
-                if (schema == null) {
-                    JsonNode schemaObject = JsonMapper.readTree(getSchema());
-                    JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
-                    schema = factory.getJsonSchema(schemaObject);
-                }
+        try {
+            payload = debugByUrl(request, response, url);
+        }
+        catch (Exception e) {
+            error(response);
+        }
 
-                JsonNode spec = readNode(inputDoc);
-                String version = getVersion(spec);
-                if (version != null && version.startsWith("\"1.")) {
+        if(payload.getMessages() == null && payload.getSchemaValidationMessages() == null) {
+            success(response);
+        }
+
+        if(payload.getSchemaValidationMessages() != null) {
+            for(SchemaValidationError message : payload.getSchemaValidationMessages()) {
+                if(INVALID_VERSION.equals(message)) {
                     upgrade(response);
-                    return;
                 }
-
-                ProcessingReport report = schema.validate(spec);
-                if (report.isSuccess()) {
-                    Swagger swagger = readSwagger(inputDoc);
-                    if (swagger != null) {
-                        success(response);
-                    } else {
-                        fail(response);
-                    }
-                } else {
-                    fail(response);
-                }
-            } catch (Exception e) {
-                LOGGER.trace("failed to verify by URL: " + url, e);
-                error(response);
             }
         }
+
+        error(response);
     }
 
     private String getVersion(JsonNode node) {
@@ -109,42 +96,51 @@ public class ValidatorService {
         return null;
     }
 
-    public List<SchemaValidationError> debugByUrl(HttpServletRequest request, HttpServletResponse response, String url) throws Exception {
-        List<SchemaValidationError> output = new ArrayList<SchemaValidationError>();
-
+    public ResponsePayload debugByUrl(HttpServletRequest request, HttpServletResponse response, String url) throws Exception {
+        ResponsePayload output = new ResponsePayload();
         String content;
 
+        // read the spec contents, bail if it fails
         try {
             content = getUrlContents(url);
         } catch (IOException e) {
             ProcessingMessage pm = new ProcessingMessage();
             pm.setLogLevel(LogLevel.ERROR);
             pm.setMessage("Can't read from file " + url);
-            output.add(new SchemaValidationError(pm.asJson()));
+            output.addValidationMessage(new SchemaValidationError(pm.asJson()));
             return output;
         }
 
+        // convert to a JsonNode
         JsonNode schemaObject = JsonMapper.readTree(getSchema());
-
         JsonNode spec = readNode(content);
         if (spec == null) {
             ProcessingMessage pm = new ProcessingMessage();
             pm.setLogLevel(LogLevel.ERROR);
             pm.setMessage("Unable to read content.  It may be invalid JSON or YAML");
-            output.add(new SchemaValidationError(pm.asJson()));
+            output.addValidationMessage(new SchemaValidationError(pm.asJson()));
             return output;
         }
 
+        // get the version, return deprecated if version 1.x
         String version = getVersion(spec);
-
         if (version != null && version.startsWith("\"1.")) {
             ProcessingMessage pm = new ProcessingMessage();
             pm.setLogLevel(LogLevel.ERROR);
-            pm.setMessage("Deprecated Swagger version.  Please visit http://swagger.io for information on upgrading to Swagger 2.0");
-            output.add(new SchemaValidationError(pm.asJson()));
+            pm.setMessage(INVALID_VERSION);
+            output.addValidationMessage(new SchemaValidationError(pm.asJson()));
             return output;
         }
 
+        // use the swagger deserializer to get human-friendly messages
+        SwaggerDeserializationResult result = readSwagger(content);
+        if(result != null) {
+            for(String message : result.getMessages()) {
+                output.addMessage(message);
+            }
+        }
+
+        // do actual JSON schema validation
         JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
         JsonSchema schema = factory.getJsonSchema(schemaObject);
         ProcessingReport report = schema.validate(spec);
@@ -153,7 +149,6 @@ public class ValidatorService {
 
         if (report.isSuccess()) {
             try {
-                SwaggerParser parser = new SwaggerParser();
                 readSwagger(content);
             } catch (IllegalArgumentException e) {
                 LOGGER.debug("can't read swagger contents", e);
@@ -161,7 +156,7 @@ public class ValidatorService {
                 ProcessingMessage pm = new ProcessingMessage();
                 pm.setLogLevel(LogLevel.ERROR);
                 pm.setMessage("unable to parse swagger from " + url);
-                output.add(new SchemaValidationError(pm.asJson()));
+                output.addValidationMessage(new SchemaValidationError(pm.asJson()));
                 return output;
             }
         }
@@ -169,17 +164,16 @@ public class ValidatorService {
         java.util.Iterator<ProcessingMessage> it = lp.iterator();
         while (it.hasNext()) {
             ProcessingMessage pm = it.next();
-            output.add(new SchemaValidationError(pm.asJson()));
+            output.addValidationMessage(new SchemaValidationError(pm.asJson()));
         }
         return output;
     }
 
-    public List<SchemaValidationError> debugByContent(HttpServletRequest request, HttpServletResponse response, String content) throws Exception {
+    public ResponsePayload debugByContent(HttpServletRequest request, HttpServletResponse response, String content) throws Exception {
         JsonNode schemaObject = JsonMapper.readTree(getSchema());
         JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
         JsonSchema schema = factory.getJsonSchema(schemaObject);
-
-        List<SchemaValidationError> output = new ArrayList<SchemaValidationError>();
+        ResponsePayload output = new ResponsePayload();
 
         JsonNode spec = readNode(content);
 
@@ -187,22 +181,41 @@ public class ValidatorService {
             ProcessingMessage pm = new ProcessingMessage();
             pm.setLogLevel(LogLevel.ERROR);
             pm.setMessage("Unable to read content.  It may be invalid JSON or YAML");
-            output.add(new SchemaValidationError(pm.asJson()));
+            output.addValidationMessage(new SchemaValidationError(pm.asJson()));
             return output;
         }
 
-        ProcessingReport report = schema.validate(spec);
+        // use the swagger deserializer to get human-friendly messages
+        SwaggerDeserializationResult result = readSwagger(content);
+        if(result != null) {
+            for(String message : result.getMessages()) {
+                output.addMessage(message);
+            }
+        }
 
+        // do actual JSON schema validation
+        ProcessingReport report = schema.validate(spec);
         ListProcessingReport lp = new ListProcessingReport();
         lp.mergeWith(report);
 
         if (report.isSuccess()) {
-            Swagger swagger = readSwagger(content);
+            try {
+                readSwagger(content);
+            } catch (IllegalArgumentException e) {
+                LOGGER.debug("can't read swagger contents", e);
+
+                ProcessingMessage pm = new ProcessingMessage();
+                pm.setLogLevel(LogLevel.ERROR);
+                pm.setMessage("unable to parse swagger from contents");
+                output.addValidationMessage(new SchemaValidationError(pm.asJson()));
+                return output;
+            }
         }
+
         java.util.Iterator<ProcessingMessage> it = lp.iterator();
         while (it.hasNext()) {
             ProcessingMessage pm = it.next();
-            output.add(new SchemaValidationError(pm.asJson()));
+            output.addValidationMessage(new SchemaValidationError(pm.asJson()));
         }
         return output;
     }
@@ -297,7 +310,12 @@ public class ValidatorService {
             final CloseableHttpResponse response = httpClient.execute(getMethod);
 
             try {
+
                 HttpEntity entity = response.getEntity();
+                StatusLine line = response.getStatusLine();
+                if(line.getStatusCode() > 299 || line.getStatusCode() < 200) {
+                    throw new IOException("failed to read swagger with code " + line.getStatusCode());
+                }
                 return EntityUtils.toString(entity, "UTF-8");
             } finally {
                 response.close();
@@ -316,10 +334,9 @@ public class ValidatorService {
         return ipAddress;
     }
 
-    private Swagger readSwagger(String content) throws IllegalArgumentException {
+    private SwaggerDeserializationResult readSwagger(String content) throws IllegalArgumentException {
         SwaggerParser parser = new SwaggerParser();
-        SwaggerDeserializationResult output = parser.readWithInfo(content);
-        return output.getSwagger();
+        return parser.readWithInfo(content);
     }
 
     private JsonNode readNode(String text) {
